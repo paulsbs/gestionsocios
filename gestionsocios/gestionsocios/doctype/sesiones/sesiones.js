1c9c8efc8848299623cd786f12af9e2fc7bda190
paul criollo <paulsebascriollo@gmail.com>
2019-08-29 12:11:54 -0500

    avance

--- gestionsocios/gestionsocios/doctype/sesiones/sesiones.js
+++ gestionsocios/gestionsocios/doctype/sesiones/sesiones.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('SESIONES', {
-	refresh: function(frm) {
-
+	onload: function(frm) {
+		frm.set_query("socio", "socioasistencia", function(doc, cdt, cdn) {
+			//const row = locals[cdt][cdn];
+			return {
+			  query:
+				"gestionsocios.gestionsocios.doctype.sesiones.sesiones.get_encargado",
+			  filters: {
+				//listaprecio: frm.doc.listaprecios,
+				//almacen: frm.doc.estab
+			  }
+			};
+		  });
 	}
 });
--- gestionsocios/gestionsocios/doctype/sesiones/sesiones.py
+++ gestionsocios/gestionsocios/doctype/sesiones/sesiones.py
@@ -8,3 +8,11 @@ from frappe.model.document import Document
 
 class SESIONES(Document):
 	pass
+@frappe.whitelist()     
+def get_encargado( doctype, txt, searchfield, start, page_len, filters):
+      
+        #return frappe.db.sql(""" select  p.name, nombre, codigo, unidad , pa.cantidad, pa.almacen, lp.precio, lp.incluyeiva, lp.ivaporcentaje
+        return frappe.db.sql(""" SELECT 
+distinct `tabLOCAL`.encargado 
+FROM 
+`tabLOCAL` """ )
