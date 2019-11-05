880db491a441c268866a0bad7a1996f02e2c9d4e
paulsbs <paulsebascriollo@gmail.com>
2019-11-05 10:58:39 -0500

    cambios

--- gestionsocios/gestionsocios/doctype/tarjetas_coutas/tarjetas_coutas.js
+++ gestionsocios/gestionsocios/doctype/tarjetas_coutas/tarjetas_coutas.js
@@ -7,6 +7,9 @@ console.log('a');
   },
 	 socio: function(frm) {
       getDatos(frm);
+    },
+    btncuotas:function(frm){
+      generarCuotas(frm);
     }
 	
 });
@@ -23,4 +26,60 @@ function getDatos(frm) {
 
       }
     });
+}
+
+function generarCuotas(frm){
+ 
+
+  frappe.prompt([
+    {
+      fieldname: "anio",
+      label: __("Año"),
+      fieldtype: "Select",
+      options: [
+        { "value": "2017", "label":"2017" },
+        { "value": "2018", "label":"2018" },
+        { "value": "2019", "label":"2019" },
+        { "value": "2021", "label":"2021" }, 
+        { "value": "2022", "label":"2022" }, 
+        { "value": "2023", "label":"2023" }, 
+        { "value": "2024", "label":"2024" }, 
+        { "value": "2025", "label":"2025" }, 
+        { "value": "2026", "label":"2026" }, 
+        { "value": "2027", "label":"2027" }, 
+        { "value": "2028", "label":"2028" }, 
+      ],
+      reqd: 1,
+      
+    } 
+    ,
+    {
+      fieldname: 'valor',
+      label: __('Valor'),
+      fieldtype: 'Link',
+      reqd: 1,
+      options: 'valor' 
+    }
+  ], (data) => {
+     console.log(data); 
+     frappe.call({				 
+      doc: frm.doc,
+      method: "generarpago",
+      args: {
+        anio:data.anio,
+        socio:frm.doc.socio,
+        valor:data.valor,
+       },
+      callback: r => {      
+        console.log(r);   
+        frappe.show_alert({
+              indicator: 'green',
+              message: __('Pagos Generados')
+            });
+      }
+      });
+  });
+
+ 
+
 }
--- gestionsocios/gestionsocios/doctype/tarjetas_coutas/tarjetas_coutas.py
+++ gestionsocios/gestionsocios/doctype/tarjetas_coutas/tarjetas_coutas.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+ 
 
 class TarjetasCoutas(Document):
 	def getDatosCliente(self,nombre):        
@@ -12,3 +13,14 @@ class TarjetasCoutas(Document):
 			datossocio =  frappe.db.sql(""" SELECT s.cedula , l.name   FROM  tabSOCIOS s inner join tabLOCAL l on ( s.name= l.encargado) where s.name=%s""",nombre)[0]
 		return datossocio
 
+	def generarpago(self,anio,socio,valor):        
+		if self and valor:
+			meses = ["SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE","ENERO", "FEBRERO", "MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO"]
+			for m in meses:
+				pagos = frappe.get_doc("Tarjetas Coutas",self.name)
+				row = pagos.append("mes_socio", {}) 			 
+				row.mes = m
+				row.valor =valor			 
+				row.anio = anio
+				row.insert()
+		return "Exito"
